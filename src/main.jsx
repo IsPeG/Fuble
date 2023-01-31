1866945c2057bb6fb7ecf7dd1a489a916caca81b
IsPeG <igr13267@gmail.com>
2023-01-31 11:25:23 +0100

    gitignore fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,5 +6,5 @@ const App = React.lazy(() => import('./App'));
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Suspense fallback={<h1>Cargando...</h1>}>
     <App />
-  </React.Suspense>,
+  </React.Suspense>
 )
