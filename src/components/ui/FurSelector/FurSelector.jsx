dc89ffbaa84248e13b50285efc03416e17dc476c
IsPeG <igr13267@gmail.com>
2023-06-07 00:13:52 +0200

    Added filters for furniture select

--- src/components/ui/FurSelector/FurSelector.jsx
+++ src/components/ui/FurSelector/FurSelector.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import FurSelectorItem from "../FurSelectorItem/FurSelectorItem";
 
 import furnitureData from "../../../_furnitureData/data.json";
@@ -6,39 +6,35 @@ import furnitureData from "../../../_furnitureData/data.json";
 import "./furSelector.css";
 
 export default function FurSelector(props) {
+  const [filter, setFilter] = useState(null);
   const furData = [...furnitureData];
 
+  console.log(filter);
+
   const Filters = () => {
+    const filtersTypes = ["sit", "table", "plant", "light", "misc"];
+
+    const handleFilterClick = (filterName) => {
+      setFilter(filter == filterName ? null : filterName);
+    };
+
     return (
       <div className="filtersContainer">
-        <div className="furSelectorItem">
-          <img
-            className="furSelectorItemImage"
-            src={`/src/assets/textures/ui/icons/filters/filter1.png`}
-            alt="icon"
-          />
-        </div>
-        <div className="furSelectorItem">
-          <img
-            className="furSelectorItemImage"
-            src={`/src/assets/textures/ui/icons/filters/filter2.png`}
-            alt="icon"
-          />
-        </div>
-        <div className="furSelectorItem">
-          <img
-            className="furSelectorItemImage"
-            src={`/src/assets/textures/ui/icons/filters/filter3.png`}
-            alt="icon"
-          />
-        </div>
-        <div className="furSelectorItem">
-          <img
-            className="furSelectorItemImage"
-            src={`/src/assets/textures/ui/icons/filters/filter4.png`}
-            alt="icon"
-          />
-        </div>
+        {filtersTypes.map((element, key) => (
+          <div
+            id={`${element}FilterButton`}
+            className={
+              filter == element ? "furSelectorItem selected" : "furSelectorItem"
+            }
+            onClick={(e) => handleFilterClick(element)}
+          >
+            <img
+              className="furSelectorItemImage"
+              src={`/src/assets/textures/ui/icons/filters/filter${key}.png`}
+              alt="icon"
+            />
+          </div>
+        ))}
       </div>
     );
   };
@@ -48,15 +44,27 @@ export default function FurSelector(props) {
       <div className="furSelectorWrapper">
         <Filters />
         <div className="furSelector">
-          {furData.map((furniture, key) => (
-            <FurSelectorItem
-              key={key}
-              furId={furniture.id}
-              furName={furniture.name}
-              furSize={furniture.size}
-              handleItemClick={props.handleItemClick}
-            />
-          ))}
+          {filter
+            ? furData.map((furniture, key) =>
+                filter == furniture.type ? (
+                  <FurSelectorItem
+                    key={key}
+                    furId={furniture.id}
+                    furName={furniture.name}
+                    furSize={furniture.size}
+                    handleItemClick={props.handleItemClick}
+                  />
+                ) : null
+              )
+            : furData.map((furniture, key) => (
+                <FurSelectorItem
+                  key={key}
+                  furId={furniture.id}
+                  furName={furniture.name}
+                  furSize={furniture.size}
+                  handleItemClick={props.handleItemClick}
+                />
+              ))}
         </div>
       </div>
     </>
